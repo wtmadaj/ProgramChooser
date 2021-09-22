e1b0adc5a1504feab430eda56f99b44a69c99681
Wes Madaj <wtmadaj@gmail.com>
2021-09-22 16:53:32 -0400

    added handling of negative numbers to IntegerReversal

--- src/main/java/IntegerReversal.java
+++ src/main/java/IntegerReversal.java
@@ -6,13 +6,20 @@ public class IntegerReversal {
         System.out.println("Enter a number and I'll reverse it: ");
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
+        int sign = 0;
+        if (x<0) {
+            sign = -1;
+        } else { sign = 1; }
 
         StringBuilder y = new StringBuilder(String.valueOf(x));
         y.reverse();
         String z = y.toString();
+        if (z.endsWith("-")) {
+            z = z.substring(0, z.length()-1);
+        }
         int a = Integer.parseInt(z);
 
-        System.out.println(a);
+        System.out.println(a*sign);
 
 //        TODO:
 //          -Add exception handling for numbers smaller than -2^31 and larger than 2^31-1
