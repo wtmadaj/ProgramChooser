7d61b2e324a0726cea25edf5216fbbae287922d6
Wes Madaj <wtmadaj@gmail.com>
2021-11-13 06:45:43 -0500

    Starting string reversal class, updated gitignore

---
+++ src/main/java/StringReversal.java
@@ -0,0 +1,10 @@
+public class StringReversal {
+    public void reverseString(char[] s) {
+        char temp;
+        for(int i=0; i<s.length/2; i++) {
+            temp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = temp;
+        }
+    }
+}
